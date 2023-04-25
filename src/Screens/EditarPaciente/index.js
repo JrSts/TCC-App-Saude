634233d7a3df7ed6acf65a511f2f7304f91d6760
JrSts <jrs.ifs@gmail.com>
2023-04-25 11:49:32 -0300

    Correção do ID do paciente

--- src/Screens/EditarPaciente/index.js
+++ src/Screens/EditarPaciente/index.js
@@ -1,41 +1,43 @@
-import { SafeAreaView, Text, View } from 'react-native'
+import { Alert, SafeAreaView, Text, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import TitleBar from '../../Components/TitleBar'
 import styles from './styles'
 import Input from '../../Components/Input'
 import Firestore from '@react-native-firebase/firestore'
 import Button from '../../Components/Button'
+import { useNavigation } from '@react-navigation/native'
 
 export default function EditarPaciente({route}) {
 
+  const navigation = useNavigation()
   const id = route.params.id
-  const [idPaciente, setIdPaciente] = useState('')
   const [hipotese, setHipotese] = useState('')
   const [nome, setNome] = useState('')
 
   useEffect(() => {
     const subscriber = Firestore()
     .collection('Pacientes')
-    .where('id', '==', id)
     .onSnapshot(query => query.docs.map(doc => {
-      setHipotese(doc.data().hipotese)
-      setIdPaciente(doc.id)
-      setNome(doc.data().nome)
+      if (doc.id == id){
+        setHipotese(doc.data().hipotese)
+        setNome(doc.data().nome)
+      }
     }))
     return () => subscriber()
   }, [])
   
   function atualizarPaciente() {
-    Firestore().collection('Pacientes').doc(idPaciente).update({
+    Firestore().collection('Pacientes').doc(id).update({
       hipotese
-    })
+    }).then(() => Alert.alert('Paciente editado com sucesso!!'))
+    navigation.goBack()
   }
 
   return (
     <SafeAreaView style={styles.container}>
       <TitleBar title='Editar Paciente'/>
       <View style={styles.content}>
-        <Text style={styles.label}>Atualize a Hipótese Diagnóstica do de {nome}:</Text>
+        <Text style={styles.label}>Atualize a Hipótese Diagnóstica de {nome}:</Text>
         <Input 
           title='Hipotese Diagnostica' 
           value={hipotese} 
